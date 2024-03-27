a7c6b5b9976013e8da2ed34f448b95e06356e840
Michalis Kamburelis <michalis.kambi@gmail.com>
2024-03-27 14:35:59 +0000

    Display Unity widget from Flutter on iOS

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_unity_widget/flutter_unity_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +32,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'dfasdfasf asdfFlutter Demo Home Page'),
+      home:
+          //const MyHomePage(title: 'dfasdfasf asdfFlutter Demo Home Page'),
+          const UnityDemoScreen(),
     );
   }
 }
@@ -121,5 +124,45 @@ class _MyHomePageState extends State<MyHomePage> {
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class UnityDemoScreen extends StatefulWidget {
+  const UnityDemoScreen({Key? key}) : super(key: key);
+
+  @override
+  State<UnityDemoScreen> createState() => _UnityDemoScreenState();
+}
+
+class _UnityDemoScreenState extends State<UnityDemoScreen> {
+  static final GlobalKey<ScaffoldState> _scaffoldKey =
+      GlobalKey<ScaffoldState>();
+  UnityWidgetController? _unityWidgetController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      body: SafeArea(
+        bottom: false,
+        child: WillPopScope(
+          onWillPop: () async {
+            // Pop the category page if Android back button is pressed.
+            return true;
+          },
+          child: Container(
+            color: Colors.yellow,
+            child: UnityWidget(
+              onUnityCreated: onUnityCreated,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  // Callback that connects the created controller to the unity controller
+  void onUnityCreated(controller) {
+    _unityWidgetController = controller;
   }
 }
