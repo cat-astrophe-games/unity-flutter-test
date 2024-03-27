0d7f7aedb88e9f640b109fa9843fa80dad802d7d
Michalis Kamburelis <michalis.kambi@gmail.com>
2024-03-27 15:43:00 +0000

    2 pages - first and unity

--- lib/main.dart
+++ lib/main.dart
@@ -32,14 +32,16 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'dfasdfasf asdfFlutter Demo Home Page'),
-      //const UnityDemoScreen(),
+      home:
+          //const MyHomePage(title: 'dfasdfasf asdfFlutter Demo Home Page'),
+          //const UnityDemoScreen(),
+          const FirstPage(title: 'First Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class FirstPage extends StatefulWidget {
+  const FirstPage({super.key, required this.title});
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -53,10 +55,90 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<FirstPage> createState() => _FirstPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _FirstPageState extends State<FirstPage> {
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the FirstPage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Press the button to load Unity',
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: buttonPressed,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  void buttonPressed() {
+    // https://docs.flutter.dev/cookbook/navigation/navigation-basics
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (context) => const UnityPage(title: 'Unity Page')),
+    );
+  }
+}
+
+class UnityPage extends StatefulWidget {
+  const UnityPage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<UnityPage> createState() => _UnityPageState();
+}
+
+class _UnityPageState extends State<UnityPage> {
   //int _counter = 0;
 
   UnityWidgetController? _unityWidgetController;
@@ -76,7 +158,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
         // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
+        // Here we take the value from the UnityPage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
@@ -118,6 +200,10 @@ class _MyHomePageState extends State<MyHomePage> {
                       min: 0,
                       max: 20,
                     ),
+                    ElevatedButton(
+                      onPressed: buttonBackPressed,
+                      child: const Text('Back'),
+                    ),
                   ],
                 ),
               ),
@@ -126,6 +212,11 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void buttonBackPressed() {
+    // see https://docs.flutter.dev/cookbook/navigation/navigation-basics
+    Navigator.pop(context);
   }
 
   // Communcation from Flutter to Unity
@@ -139,7 +230,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   // Communication from Unity to Flutter
   void onUnityMessage(message) {
-    print('Received message from unity: ${message.toString()}');
+    debugPrint('Received message from unity: ${message.toString()}');
   }
 
   // Callback that connects the created controller to the unity controller
@@ -150,13 +241,14 @@ class _MyHomePageState extends State<MyHomePage> {
   // Communication from Unity when new scene is loaded to Flutter
   void onUnitySceneLoaded(SceneLoaded? sceneInfo) {
     if (sceneInfo != null) {
-      print('Received scene loaded from unity: ${sceneInfo.name}');
-      print(
+      debugPrint('Received scene loaded from unity: ${sceneInfo.name}');
+      debugPrint(
           'Received scene loaded from unity buildIndex: ${sceneInfo.buildIndex}');
     }
   }
 }
 
+/*
 class UnityDemoScreen extends StatefulWidget {
   const UnityDemoScreen({Key? key}) : super(key: key);
 
@@ -196,3 +288,4 @@ class _UnityDemoScreenState extends State<UnityDemoScreen> {
     _unityWidgetController = controller;
   }
 }
+*/
